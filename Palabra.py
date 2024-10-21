65320bd93bafd3c57ed6f118b966362e710f4fd0
Odalys Silva Colli <margely2604@hotmail.com>
2024-10-21 11:42:08 -0600

    Update Palabra.py

--- Palabra.py
+++ Palabra.py
@@ -1,19 +1,20 @@
 from collections import deque
 
-# Definir la lista como una deque (doble cola)
-lista = deque()
+lista = deque() # Definir la lista como una deque (doble cola)
 
 # Añadir elementos a la lista
 lista.append("Palabra")
+print(lista)
 lista.append(5)
+print(lista)
 lista.append(17)
+print(lista)
 lista.append("Palabra 2")
-
-# Eliminar el último elemento
-lista.pop()
-
-# Mostrar el tamaño de la lista
+print(lista)
 print("El tamaño de la lista es:", len(lista))
 
-# Mostrar el último elemento de la lista
-print(lista[-1])
+lista.pop() # Eliminar el último elemento
+
+print("El tamaño de la lista al eliminar el ultimo elemento:", len(lista)) # Mostrar el tamaño de la lista
+
+print(lista[-1]) # Mostrar el último elemento de la lista
